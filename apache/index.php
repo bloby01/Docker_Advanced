36ec72c12fbc842d18a516a42b9b25b789fb12cd
bloby01 <57810141+bloby01@users.noreply.github.com>
2020-04-01 14:40:49 +0200

    Add files via upload

---
+++ apache/index.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Hello ! ";
+    echo " ";
+    echo "IP serveur : ".$_SERVER["SERVER_ADDR"]."<br/>";
+    echo "Url : ".$_SERVER["REQUEST_URI"]."<br/>";
+    echo " ";
+    echo "*****************************";
+    echo " ";
+?>
