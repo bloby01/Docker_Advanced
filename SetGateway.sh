f0057cddaaf045fb0467fa5e9a36b8b5162332a5
blob <57810141+bloby01@users.noreply.github.com>
2021-11-18 10:28:31 +0100

    Delete SetGateway.sh

--- SetGateway.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-clear
-echo -n "Mettre l'adresse ip de la gateway: "
-read gateway
-ip route add 0.0.0.0/0 via $gateway
-echo " " ; echo "###########"
-echo "" ; echo "Liste des routes disponibles"
-ip route
