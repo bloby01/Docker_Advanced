c01d922c53ffa8086169d6eadef747a1dfbd1ca5
blob <57810141+bloby01@users.noreply.github.com>
2021-11-18 10:31:01 +0100

    Delete index.php

--- apache/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "Hello ! ";
-    echo " ";
-    echo "IP serveur : ".$_SERVER["SERVER_ADDR"]."<br/>";
-    echo "Url : ".$_SERVER["REQUEST_URI"]."<br/>";
-    echo " ";
-    echo "*****************************";
-    echo " ";
-?>
